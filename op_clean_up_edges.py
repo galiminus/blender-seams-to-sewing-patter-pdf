227f40c17748b03b04b431b0d72c9b62c1f0852e
Thomas Kole <thomas@thomas-desktop>
2020-10-13 21:14:40 +0200

    Even better edge cleanup

--- op_clean_up_edges.py
+++ op_clean_up_edges.py
@@ -1,6 +1,7 @@
 import bpy
 import bmesh
 import random
+import mathutils
 from bpy.types import Operator
 from bpy.props import (
     BoolProperty,
@@ -10,6 +11,13 @@ from bpy.props import (
     IntProperty,
 )
 
+class FakeEdge:
+  v1 = None
+  v2 = None
+
+class FakeVert:
+  pos = None
+  verts = None
 
 class ObjectModeOperator:
     @classmethod
@@ -22,174 +30,216 @@ class CleanUpEdges(bpy.types.Operator):
     bl_label = "Clean up edges"
     bl_options = {'REGISTER', 'UNDO'}
 
-    loop_relax_it: EnumProperty(
-        name="Relax Iterations",
-        items=(("1", "1", "One"),
-               ("3", "3", "Three"),
-               ("5", "5", "Five"),
-               ("10", "10", "Ten"),
-              ("25", "25", "Twenty-five")),
-        description="Number of times the loop is relaxed",
-        default="1"
-        )
-
-    relax_size: IntProperty(
-        name="Smooth vertex neighbor size:",
-        description="After the seam has been relaxed, neighboring vertices are smoothed.",
-        default=1,
-        min=0,
-        soft_max=5
+    delimit_boundary: BoolProperty(
+        default=True
+    )
+    delimit_existing_seams: BoolProperty(
+        default=True
+    )
+    delimit_intersections: BoolProperty(
+        default=True
     )
 
     min_length: FloatProperty(
-        name="Minimum Edge Length",
-        description="Collapse edges under this length",
-        default=0.002,
+        default=0.02,
         min=0,
-        soft_max=0.1
+        soft_max=0.5
+    )
+
+    relax_iterations: IntProperty(
+        default=3,
+        min=0,
+        soft_max=20
+    )
+
+    neighbor_selection_radius: IntProperty(
+        default=1,
+        min=1,
+        soft_max=20
+    )
+
+    neighbor_smooth_factor: FloatProperty(
+        default=0.5,
+        min=0,
+        max=1
     )
 
     def execute(self, context):
 
         obj = bpy.context.edit_object
         me = obj.data
-        mesh = bmesh.from_edit_mesh(me)
+        bm = bmesh.from_edit_mesh(me)
+
+        bpy.ops.mesh.select_mode(type="EDGE")
+
+        max_it = len(list(filter(lambda e: e.select, bm.edges)))
+        edges = list(filter(lambda e: e.select, bm.edges))
+
+        if self.delimit_intersections:
+            for e in edges:
+                for v in e.verts:
+                    star_count = 0
+                    for le in v.link_edges:
+                        if le.select:
+                            star_count += 1
+                    if star_count > 2:
+                        e.select = False
+                        break
+
+        if self.delimit_existing_seams:
+            for e in edges:
+                for v in e.verts:
+                    for le in v.link_edges:
+                        if le.seam and not (le in edges):
+                            e.select = False
+                            break
 
 
-        seam_edges = list(filter(lambda e: e.seam, mesh.edges))
-        to_relax = []
-        for e in seam_edges:
-            is_star = False
+        if self.delimit_boundary:
+            for e in edges:
+                for v in e.verts:
+                    if v.is_boundary:
+                        e.select = False
+                        break
+
+        edges = list(filter(lambda e: e.select, bm.edges))
+        '''
+        edges.sort(key=lambda e: e.calc_length())
+        for _ in range(max_it):
+            edges = list(filter(lambda e: e.select, bm.edges))
+            if (len(edges) is 0):
+                return {'FINISHED'}
+            shortest = min(edges, key=lambda e: e.calc_length())
+            if (shortest.calc_length() < self.min_length):
+                to_collapse = []
+                to_collapse.append(shortest)
+                bmesh.ops.collapse(bm, edges=to_collapse, uvs=True)
+        '''
+        fake_verts = dict()
+        for e in edges:
             for v in e.verts:
-                if v.is_boundary:
-                    is_star = True
-                star_count = 0
-                for ce in v.link_edges:
-                    if ce.seam:
-                        star_count += 1
-                if star_count > 2:
-                    is_star = True
-            if not is_star:
-                to_relax.append(e)
+                fake_vert = FakeVert()
+                fake_vert = FakeVert()
+                fake_vert.verts = []
+                fake_vert.verts.append(v)
+                fake_vert.pos = v.co
+                fake_verts[v] = fake_vert
 
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.mesh.dissolve_degenerate(threshold=0.0001)
-        bpy.ops.mesh.select_all(action='DESELECT')
+        fake_vert_list = []
+        fake_vert_list.extend(fake_verts.values())
+        for fv in fake_vert_list:
+            print(fv)
 
-        bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='DISABLE')
-        bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='FACE', action='DISABLE')
-        bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='ENABLE')
-        bpy.ops.mesh.select_all(action='DESELECT')
+        fake_edges = []
+        for e in edges:
+            fake_edge = FakeEdge()
+            fake_edge.v1 = fake_verts[e.link_loops[0].vert]
+            fake_edge.v2 = fake_verts[e.link_loops[0]. link_loop_next.vert]
+            fake_edges.append(fake_edge)
 
-        for e in to_relax:
-            e.select = True;
+        for _ in range(max_it):
+            if (len(fake_edges) <= 1):
+                break;
+            shortest = min(fake_edges, key=lambda e: (e.v1.pos - e.v2.pos).length)
+            if ((shortest.v1.pos - shortest.v2.pos).length < self.min_length):
+                fake_vert_list.remove(shortest.v1)
+                fake_vert_list.remove(shortest.v2)
+                new_fake_vert = FakeVert()
+                new_fake_vert.pos = (shortest.v1.pos + shortest.v2.pos)/2
+                new_fake_vert.verts = []
+                new_fake_vert.verts.extend(shortest.v1.verts)
+                new_fake_vert.verts.extend(shortest.v2.verts)
+                fake_vert_list.append(new_fake_vert)
+                fake_edges.remove(shortest)
+                for fe in fake_edges:
+                    if (fe.v1 is shortest.v1):
+                        fe.v1 = new_fake_vert
+                    if (fe.v2 is shortest.v2):
+                        fe.v2 = new_fake_vert
+                    if (fe.v1 is shortest.v2):
+                        fe.v1 = new_fake_vert
+                    if (fe.v2 is shortest.v1):
+                        fe.v2 = new_fake_vert
 
-        bpy.ops.mesh.looptools_relax(regular=False, iterations=self.loop_relax_it, interpolation='cubic') #todo: detect
-        bpy.ops.mesh.dissolve_degenerate(threshold=self.min_length)
+        for fv in fake_vert_list:
+            print(fv.pos)
+            for v in fv.verts:
+                v.co = fv.pos
 
-        bmesh.update_edit_mesh(me)
-        mesh = bmesh.from_edit_mesh(me)
+        bpy.ops.mesh.remove_doubles()
 
-
-
-        if (self.relax_size > 0):
-
-            selected_edges = list(filter(lambda e: e.select, mesh.edges))
-
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='DISABLE')
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='ENABLE')
-
-            for x in range(self.relax_size):
-                bpy.ops.mesh.select_more(use_face_step=False)
-
-
-            for e in selected_edges:
-                for v in e.verts:
-                    v.select = False;
-
-            selected_verts = list(filter(lambda v: v.select, mesh.verts))
-            for v in selected_verts:
-                if v.is_boundary:
-                    v.select = False
-
-            bpy.ops.mesh.vertices_smooth(factor=0.2, repeat=10)
-            bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.dissolve_degenerate(threshold=0.0001)
-            bpy.ops.mesh.select_all(action='DESELECT')
-
-        bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='ENABLE')
-        bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='DISABLE')
-
-        bmesh.update_edit_mesh(me)
-        mesh = bmesh.from_edit_mesh(me)
-
+        print(len(edges))
 
         '''
-        for x in range(self.iterations):
 
-            seam_edges = list(filter(lambda e: e.seam, mesh.edges))
+        for _ in range(max_it):
+            shortest = min(edges, key=lambda e: e.calc_length())
+            if (shortest.calc_length() < self.min_length):
+                avg = mathutils.Vector()
+                for v in shortest.verts:
+                    avg += v.co
+                avg /= 2
+                for v in shortest.verts:
+                    v.co = avg
+                edges.remove(shortest)
+        '''
 
-            samples = int(len(seam_edges) / self.iterations)
-            samples *= 2
-            sublist = random.sample(seam_edges, int(samples))
-            toremove = []
-            for e in sublist:
-                if e.calc_length() < self.min_length:
-                    is_star = False
+        selection = list(filter(lambda e: e.select, bm.edges))
+
+        for _ in range(self.relax_iterations):
+            verts = list(filter(lambda v: v.select, bm.verts))
+            locations = dict()
+            for v in verts:
+                locations[v] = v.co.copy()
+
+            for v in verts:
+                neighbors = []
+                for l in v.link_loops:
+                    if (l.edge.select):
+                        neighbors.append(l.link_loop_next.vert)
+                if (len(neighbors) == 2):
+                    avg_pos = mathutils.Vector()
+                    for n in neighbors:
+                        avg_pos += locations[n]
+                    avg_pos /= len(neighbors)
+                    v.co = v.co.lerp(avg_pos, 0.2)
+
+        for _ in range(self.neighbor_selection_radius):
+            bpy.ops.mesh.select_more(use_face_step=True)
+
+        if self.delimit_existing_seams:
+            sel = list(filter(lambda e: e.select, bm.edges))
+            for e in sel:
+                if e.seam:
                     for v in e.verts:
-                        if v.is_boundary:
-                            is_star = True
-                        star_count = 0
-                        for ce in v.link_edges:
-                            if ce.seam:
-                                star_count += 1
-                        if star_count > 2:
-                            is_star = True
-                    if not is_star:
-                        toremove.append(e)
+                        v.select = False
 
-            if (len(toremove) > 0):
-                bmesh.ops.collapse(mesh, edges = toremove, uvs = True)
+
+        selected_verts = list(filter(lambda v: v.select, bm.verts))
+        verts_to_smooth = []
+
+        for v in selected_verts:
+            if not(v.is_boundary and self.delimit_boundary):
+                verts_to_smooth.append(v)
+
+        print(len(verts_to_smooth))
+
+        for e in selection:
+            for v in e.verts:
+                while v in verts_to_smooth:
+                    verts_to_smooth.remove(v)
+
+        smoothing_factor = self.neighbor_smooth_factor
+        smoothing_factor = pow(smoothing_factor, 4)
+        smoothing_factor /= 2
+
+        for x in range(10):
+            bmesh.ops.smooth_vert(bm, verts=verts_to_smooth, factor= smoothing_factor, use_axis_x=True, use_axis_y=True, use_axis_z=True)
+            print("smooth")
 
         bmesh.update_edit_mesh(me)
-        mesh = bmesh.from_edit_mesh(me)
-        '''
-        '''
-        if self.use_dissolve:
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='DISABLE')
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='FACE', action='DISABLE')
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='ENABLE')
-            bpy.ops.mesh.select_all(action='DESELECT')
-            seam_edges = list(filter(lambda e: e.seam, mesh.edges))
-            for e in seam_edges:
-                e.select = True;
-
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE', action='DISABLE')
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='ENABLE')
-
-            bpy.ops.mesh.select_more(use_face_step=False)
-
-            for e in seam_edges:
-                for v in e.verts:
-                        v.select = False;
-
-            bpy.ops.mesh.dissolve_limited(angle_limit=0.6, use_dissolve_boundaries=False, delimit={'SEAM'})
-            bmesh.update_edit_mesh(me)
-            mesh = bmesh.from_edit_mesh(me)
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT', action='DISABLE')
-            bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='FACE', action='ENABLE')
-            bpy.ops.mesh.select_all(action='DESELECT')
-
-            for e in seam_edges:
-                for f in e.link_faces:
-                        f.select = True;
-
-            bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
-            bpy.ops.mesh.tris_convert_to_quads(face_threshold=0.698132, shape_threshold=0.698132, uvs=True, vcols=False, seam=True, sharp=True, materials=True)
-
-        '''
-
-        #bpy.ops.mesh.select_all(action='DESELECT')
+        bm.free()
+        me.update()
 
         return {'FINISHED'}
 
